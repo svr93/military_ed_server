b7d22edbcbe0a0bdb8fd1125a0b59fc779258766
svr93 <svr93@i.ua>
2015-03-29 21:22:59 +0300

    add_addon_using_example

--- applications/main/app/api/info.json/get.js
+++ applications/main/app/api/info.json/get.js
@@ -1,5 +1,11 @@
 module.exports = function(client, callback) {
+  
+  var addon = require(process.cwd() +
+  '/applications/main/app/api/info.json/build/Release/addon');
+
   console.log(client.query);
+  
+  console.log(addon.add(7, 11));
   
   client.context.data = {
     result: 'OK'
