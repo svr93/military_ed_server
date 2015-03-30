242dacfe6ade670cbe1a849651b8c2ac53146b52
svr93 <svr93@i.ua>
2015-03-30 03:47:06 +0300

    math_space.h___1.0

--- applications/main/app/api/info.json/math_space.h
+++ applications/main/app/api/info.json/math_space.h
@@ -3,4 +3,25 @@
 
 #include <math.h>
 
+#define pi M_PI
+#define pi2 (M_PI * 2)
+#define fDegreeInRadian (180.0/M_PI)
+#define fDegreeInGrad (M_PI/180.0)
+#define R_Earth 6371
+#define fSecondsInDay 86400.0
+#define V_ugl (M_PI * 2.0 / fSecondsInDay)
+#define J_1957 2436204.5
+#define Rz (R_Earth * 1000)
+#define Mu (398600.44e9 * pow(fSecondsInDay, 2.0) / pow(Rz, 3.0))
+#define MU_km_s 398600.44
+
+double GMST(double t);
+double abs_my(double val);
+
+void LBtoGSK(double& dolgota, double& shirota,
+double& h, double& Xgsk,double& Ygsk,double& Zgsk);
+
+void GSKtoNSK(double & Xgsk, double & Ygsk, double & Zgsk,
+double & Xnsk, double & Ynsk, double & Znsk, double& t);
+
 #endif
