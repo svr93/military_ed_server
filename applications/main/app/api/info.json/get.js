43d1fe547560eb5070794ede495f4395eedecf16
Владимир <svr93@i.ua>
2015-04-07 20:13:29 +0400

    fix_errors

--- applications/main/app/api/info.json/get.js
+++ applications/main/app/api/info.json/get.js
@@ -3,6 +3,9 @@ module.exports = function(client, callback) {
   var addon = require(process.cwd() +
   '/applications/main/app/api/info.json/build/Release/addon');
 
+  var mathLib = require(process.cwd() +
+  '/applications/main/app/api/info.json/additional_math_funcs.js');
+
   console.log(client.query);
   
   var objX = 6371;
@@ -15,10 +18,16 @@ module.exports = function(client, callback) {
   console.log(addon.calculateRelativeCoords(objX, objY, objZ,
                                             stLatitude, stLongitude));
 
+  var apogeeHeight = 918.1;
+  var perigeeHeight = 410.2;
+
+  var eccentricity = mathLib.getEccentricity(perigeeHeight, apogeeHeight);
+  var semiMajorAxis = mathLib.getSemiMajorAxis(perigeeHeight, eccentricity);
+
   console.log(addon.calculateSatellitePos({
 
-    apogee: 918.1,
-    perigee: 410.2,
+    semiMajorAxis: semiMajorAxis,
+    eccentricity: eccentricity,
     inclination: 97.86907,
     ascendingNodeLongitude: 205.44564,
     perigeeArg: 29.15896,
