1835133910b4c22fd4690896b1f0d444af211449
Владимир <svr93@i.ua>
2015-04-08 20:06:32 +0400

    add_coords_translation_func

--- applications/main/app/api/info.json/additional_math_funcs.js
+++ applications/main/app/api/info.json/additional_math_funcs.js
@@ -19,3 +19,23 @@ exports.getSemiMajorAxis = function(perigeeHeight, eccentricity) {
 
   return (EARTH_RADIUS + perigeeHeight) / (1 - eccentricity);
 };
+
+exports.translateCartesianToGeographic = function(x, y, z) {
+  // x, y, z: km, return: degrees, km
+
+  var radial_distance = Math.sqrt(x * x + y * y + z * z);
+  var polar_angle = Math.atan(Math.sqrt(x * x + y * y) / z);
+  var azimuthal_angle = Math.atan(y / x);
+
+  var latitudeRad = Math.abs(Math.PI - polar_angle) - Math.PI / 2;
+
+  var longitudeRad = (azimuthal_angle < 2 * Math.PI) ?
+                  azimuthal_angle : azimuthal_angle - 2 * Math.PI;
+
+  return {
+    latitude: latitudeRad * 180 / Math.PI;
+    longitude: longitudeRad * 180 / Math.PI;
+    height: radial_distance - EARTH_RADIUS;
+  };
+  
+};
