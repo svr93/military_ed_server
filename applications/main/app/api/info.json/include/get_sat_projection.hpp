7a86fcff847f829366499222c0af617e6b48be48
svr93 <svr93@i.ua>
2015-04-21 18:45:55 +0300

    get_sat_projection.hpp___ver1.0

---
+++ applications/main/app/api/info.json/include/get_sat_projection.hpp
@@ -0,0 +1,12 @@
+#ifndef _GET_SAT_PROJECTION_HPP_
+#define _GET_SAT_PROJECTION_HPP_
+
+#include <node.h>
+
+#include "math_space.hpp"
+
+using namespace v8;
+
+Handle<Value> getSatProjection(const Arguments& args);
+
+#endif
