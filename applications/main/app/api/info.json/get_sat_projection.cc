01fa489f60d947cbee7a8752b3f8095acfe0802d
svr93 <svr93@i.ua>
2015-04-21 19:19:06 +0300

    get_sat_projection.cc___ver1.0

---
+++ applications/main/app/api/info.json/get_sat_projection.cc
@@ -0,0 +1,61 @@
+#define BUILDING_NODE_EXTENSION
+
+#include "include/get_sat_projection.hpp"
+
+Handle<Value> getSatProjection(const Arguments& args) {
+  HandleScope scope;
+
+  if (args.Length() < 2) {
+    ThrowException(Exception::TypeError(String::New("Wrong number of args")));
+    return scope.Close(Undefined());
+  }
+
+  if (!args[0]->IsObject() ||
+      !args[1]->IsNumber()) {
+
+    ThrowException(Exception::TypeError(String::New("Wrong args")));
+    return scope.Close(Undefined());
+  }
+
+  double t = args[1]->NumberValue(); // t: twenty-four hours
+
+  Local<Object> stl = args[0]->ToObject();
+
+  double aosk = stl->
+    Get(String::NewSymbol("stlsemimajoraxis"))->
+    NumberValue(); // aosk: km
+
+  double e = stl->
+    Get(String::NewSymbol("stleccentricity"))->
+    NumberValue();
+
+  double nakl = (stl->
+    Get(String::NewSymbol("stlincl"))->
+    NumberValue()) * fDegreeInGrad; // nakl: radians
+
+  double dby = (stl->
+    Get(String::NewSymbol("stlascendingnodelng"))->
+    NumberValue()) * fDegreeInGrad; // dby: radians
+
+  double w = (stl->
+    Get(String::NewSymbol("stlperigeearg"))->
+    NumberValue()) * fDegreeInGrad; // w: radians
+
+  double tper = 2 * M_PI * sqrt(pow(aosk, 3) / MU_km_s) / fSecondsInDay;
+  // tper: twenty-four hours
+
+  double D = 0; // radians
+  double S = 0; // radians
+  
+  PodsputnTochka(aosk, e, nakl, dby, w, tper, t, D, S);
+
+  Local<Object> res = Object::New();
+  
+  res->Set(String::NewSymbol("latitude"),
+           Number::New(S * fDegreeInRadian));
+           
+  res->Set(String::NewSymbol("longitude"),
+           Number::New(D * fDegreeInRadian));  
+
+  return scope.Close(res);
+}
